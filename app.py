35f75b110df1e9f3779e3f364ed96215057d14a4
MrSpy <88586302+MrSpy00@users.noreply.github.com>
2022-06-03 20:52:43 +0300

    app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import platform 
+
+print(platform.system())
+
+print(platform.processor())
+
+print(platform.release())
+
+print(platform.python_version())
+
+print(platform.architecture())
+
+print(platform.machine())
+
+print(platform.node())
+
+print(platform.python_branch())
+
+print(platform.system())
+
+#Bilgileri Öğren.
